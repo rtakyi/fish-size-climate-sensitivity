ac2bd4f5449e633c975408b34239fae0d7782a03
cbrown5 <c.j.brown@utas.edu.au>
2024-11-22 10:27:38 +1100

    make it run Linf and K changes at the same time

--- Scripts/library_parameters_v2.r
+++ Scripts/library_parameters_v2.r
@@ -23,25 +23,29 @@ target_species <- case_study_parameters[case_study_parameters$species == "Sardin
 n <- nrow(target_species)
 
 # Parameters for scenarios of changes in temperature, optimum temperature and growth performance due to climate change
-tempr <- seq(4, 40, by = 0.1) # temperature range (made-up numbers)
+tempr <- seq(18, 35, by = 0.1) # temperature range (made-up numbers)
 tempr_min <- 4 # minimum temperature (made-up number)
 tempr_max <- 40 # maximum temperature (made-up number)
-tempr_opt <- c(28, 29.5, 30, 32) # optimal temperature based on ipcc (CMIP6) projections (source: Sohou et al. 2020; https://www.ipcc.ch/report/ar6/wg1/downloads/factsheets/IPCC_AR6_WGI_Regional_Fact_Sheet_Australasia.pdf)
+tempr_opt <- 28
+IPCC_scnrs <- tempr_opt + c(1, 2, 3)
+
+#c(28, 29.5, 30, 32) # optimal temperature based on ipcc (CMIP6) projections (source: Sohou et al. 2020; https://www.ipcc.ch/report/ar6/wg1/downloads/factsheets/IPCC_AR6_WGI_Regional_Fact_Sheet_Australasia.pdf)
 k_opt <- 0.3 # optimal growth coefficient (made-up number)
-Fmort <- target_species$Fmort.[1] # fishing mortality rate 
 scnr_tempr <- length(tempr)
 phi_1 <- 0.0438 # growth performance per degree change in temperature (source: Kielbassa et al. 2010; Mallet et al. 1999)
 
 # Calculate indicators for each species
 
-gall <- all_scnrs <- NULL
+
 
 
 # Loop through each species
+gall <- all_scnrs <- NULL
 for (irow in 1:n){
   ## Add climate change impacts on Linf, Winf and K
   #one way to get all fmors and all temprs
-  scnr_clim <- expand.grid(tempr = tempr, Fmort = Fmort, target_species = target_species$species[irow])
+  Fmort <- target_species$Fmort.[irow]
+  scnr_clim <- expand.grid(tempr = tempr, Fmort = target_species$Fmort.[irow], target_species = target_species$species[irow])
   
   scnr_clim$size_indicator_Linf <- NA
   scnr_clim$size_indicator_Winf <- NA
@@ -62,7 +66,6 @@ for (irow in 1:n){
   L50 <- target_species$L50..cm.[irow]
   a <- 0.001 # growth performance index - this is scaling that determines units of weight. The unit of the weight is in grams, so a = 0.001
   b <- 3 # growth performance index
-  Fmort_overall <- target_species$Fmort.[irow]
   
 
   k_length <- 0.2 # slope of the logistic curve
@@ -81,19 +84,19 @@ for (irow in 1:n){
     Winf_new_clim <- Winf_tempr(Winf, Linf, Linf_new_clim)
 
     ## Calculate YPR model with new Linf growth parameters
-    dat_clim <- abundance_catch_at_age(max_age, N0, Linf_new_clim, Winf, k, t0, a, b, M, L50, k_length, Fmort_overall)
+    dat_clim <- abundance_catch_at_age(max_age, N0, Linf_new_clim, Winf, k_new_clim, t0, a, b, M, L50, k_length, Fmort)
     ind_tempr <- stock_indicators(dat_clim, Linf, Winf)
     scnr_clim$size_indicator_Linf[i] <- ind_tempr$mlength_indicator
-
-    ## Calculate YPR model with new Winf growth parameters
-    dat_clim <- abundance_catch_at_age(max_age, N0, Linf, Winf_new_clim, k, t0, a, b, M, L50, k_length, Fmort_overall)  
-    ind_tempr <- stock_indicators(dat_clim, Linf, Winf)
-    scnr_clim$size_indicator_Winf[i] <- ind_tempr$mweight_indicator
-
-    ## Calculate YPR model with new K growth parameters
-    dat_clim <- abundance_catch_at_age(max_age, N0, Linf, Winf, k_new_clim, t0, a, b, M, L50, k_length, Fmort_overall)
-    ind_tempr <- stock_indicators(dat_clim, Linf, Winf)
-    scnr_clim$size_indicator_K[i] <- ind_tempr$mlength_indicator
+    scnr_clim$Fmort <- Fmort
+    # ## Calculate YPR model with new Winf growth parameters
+    # dat_clim <- abundance_catch_at_age(max_age, N0, Linf, Winf_new_clim, k, t0, a, b, M, L50, k_length, Fmort_overall)  
+    # ind_tempr <- stock_indicators(dat_clim, Linf, Winf)
+    # scnr_clim$size_indicator_Winf[i] <- ind_tempr$mweight_indicator
+    # 
+    # ## Calculate YPR model with new K growth parameters
+    # dat_clim <- abundance_catch_at_age(max_age, N0, Linf, Winf, k_new_clim, t0, a, b, M, L50, k_length, Fmort_overall)
+    # ind_tempr <- stock_indicators(dat_clim, Linf, Winf)
+    # scnr_clim$size_indicator_K[i] <- ind_tempr$mlength_indicator
     
    }
  
@@ -105,14 +108,16 @@ for (irow in 1:n){
     # Plot results with all scenarios in one graph
     
     g2 <- ggplot(scnr_clim, aes(x = tempr)) +
-      #geom_line(aes(y = size_indicator_Linf), color = "black") +
+      geom_line(aes(y = size_indicator_Linf), color = "black") +
       #geom_line(aes(y = size_indicator_Winf), color = "blue") +
       geom_vline(xintercept = tempr_opt, linetype = 2) + 
       #geom_hline(yintercept = k_opt, linetype = 2) +
-      geom_line(aes(y = size_indicator_K), color = "red") +
+      # geom_line(aes(y = size_indicator_K), color = "red") +
       labs(title = target_species$species, x = "Temperature", y = "Size indicator") 
 
- gall <- c(gall, list(g2, target_species$species[irow]))
+    
+    
+ gall <- c(gall, list(g2))#, target_species$species[irow]))
   
 }
 
@@ -122,4 +127,11 @@ wrap_plots(gall)
 
 all_scnr_data <- bind_rows(all_scnrs)
 
+ggplot(all_scnr_data) + 
+  aes(x = tempr, y = size_indicator_Linf, color = Fmort, group = Fmort) +
+  geom_line() +
+  geom_vline(xintercept = tempr_opt, linetype = 2) +
+  geom_vline(xintercept = IPCC_scnrs, linetype = 2, col = "grey") +
+  labs(title = "Sensitivity of size indicators to changes in Linf due to climate change", x = "Temperature", y = "Size indicator") 
+  
 #ggsave(g2, filename = "Shared/Outputs/Lutjanus_sebae.tiff", width = 6, height = 6, dpi = 300)
