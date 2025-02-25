5b47a9e3608f584291abe939a187b19666060c72
rtakyi <richard.takyi@utas.edu.au>
2025-02-25 18:47:37 +1100

    Linf_new_climate code was modified to indicate a decline

--- Scripts/library_parameter_tmp_fmt_dy_v1.r
+++ Scripts/library_parameter_tmp_fmt_dy_v1.r
@@ -16,7 +16,7 @@ theme_update(axis.text.x = element_text(colour = "black", size = 12),
 source("Scripts/fish_size_functions v2.R")
 
 # Parameters
-case_study_parameters <- read.csv("Parameters/trial_slw.csv")
+case_study_parameters <- read.csv("Parameters/cs_sp_parameters_slow.csv")
 
 
 target_species <- case_study_parameters
@@ -87,8 +87,11 @@ for (irow in 1:n){
 
     #update this with linf model... 
     Linf_new_clim <- Linf_tempr(k, Linf, k_new_clim, phi_1)
-    dyna_Linf_new_clim <- Linf_new_clim + Linf_new_clim*tempr_dev[i]*phi_1
-    # dyna_Linf_new_clim <- Linf_new_clim  + (- 0.1 * tempr_dev[i] * phi_1)
+    
+    # update Linf_new_clim with climate change by reducing Linf by 1 to 5% for every degree change in temperature
+    dyna_Linf_new_clim <- Linf_new_clim - (Linf_new_clim * 0.01 * tempr_dev[i])
+
+ 
     Winf_new_clim <- a*Linf_new_clim^b
     
 
@@ -96,7 +99,6 @@ for (irow in 1:n){
     # dat_clim <- abundance_catch_at_age(max_age, N0, Linf_new_clim, Winf_new_clim, k_new_clim, t0, a, b, M, L50, k_length, Fmort)
     dat_clim <- abundance_catch_at_age(max_age, N0, dyna_Linf_new_clim, Winf_new_clim, k_new_clim, t0, a, b, M, L50, k_length, Fmort)
     ind_tempr <- stock_indicators(dat_clim, Linf, Winf)
-    # dyna_Linf_new_clim <- Linf_new_clim + Linf_new_clim*tempr_dev[i]*phi_1
     scnr_clim$size_indicator_Linf[i] <- ind_tempr$mlength_indicator
     scnr_clim$Fmort <- Fmort
     # scnr_clim$size_indicator_Winf[i] <- ind_tempr$mweight_indicator
